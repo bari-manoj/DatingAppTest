e6cf2ee0597037435a61871d9be076e58bb82905
Manoj <bari.manoj1@gmail.com>
2020-07-15 17:24:17 +0530

    Pagination & Soerting- Part - 14

--- DatingAppSPA/src/app/_resolvers/member-list.resolver.ts
+++ DatingAppSPA/src/app/_resolvers/member-list.resolver.ts
@@ -8,12 +8,14 @@ import { Injectable } from '@angular/core';
 
 @Injectable()
 export class MemberListResolver implements Resolve<User[]>{
+    pageNumber = 1;
+    pageSize = 5;
 
     constructor(private userService: UserService,
         private router: Router, private alertyfy: AlertifyService) { }
 
     resolve(route: ActivatedRouteSnapshot): Observable<User[]> {
-        return this.userService.getUsers().pipe(
+        return this.userService.getUsers(this.pageNumber, this.pageSize).pipe(
             catchError(error => {
                 this.alertyfy.error('Problem retriving data');
                 this.router.navigate(['/home']);
--- DatingAppSPA/src/app/members/member-list/member-list.component.ts
+++ DatingAppSPA/src/app/members/member-list/member-list.component.ts
@@ -1,3 +1,4 @@
+import { Pagination, PaginationResult } from './../../_models/Pagination';
 import { ActivatedRoute } from '@angular/router';
 import { AlertifyService } from '../../_services/alertify.service';
 import { UserService } from '../../_services/user.service';
@@ -11,21 +12,46 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MemberListComponent implements OnInit {
   users: User[];
-  constructor(private userService: UserService, private alertifyService: AlertifyService, 
+  user: User = JSON.parse(localStorage.getItem('user'));
+  genderList = [{ value: 'male', display: 'Males' }, { value: 'female', display: 'Females' }];
+  userParams: any = {};
+  pagination: Pagination;
+
+  constructor(private userService: UserService, private alertifyService: AlertifyService,
     private route: ActivatedRoute) { }
 
-  ngOnInit() {
+  ngOnInit(): void {
     this.route.data.subscribe(data => {
-      this.users = data['users'];
+      this.users = data['users'].result;
+      this.pagination = data['users'].pagination;
     });
+
+    this.userParams.gender = this.user.gender === 'female' ? 'male' : 'female';
+    this.userParams.minAge = 18;
+    this.userParams.maxAge = 99;
+    this.userParams.orderBy = 'lastActive';
   }
 
-  // loadUsers(): void {
-  //   this.userService.getUsers().subscribe((users: User[]) => {
-  //     this.users = users;
-  //   }, error => {
-  //     this.alertifyService.error(error);
-  //   });
-  // }
+  pageChanged(event: any): void {
+    this.pagination.currentPage = event.page;
+    this.loadUsers();
+  }
+
+  resetFilters(): void {
+    this.userParams.gender = this.user.gender === 'female' ? 'male' : 'female';
+    this.userParams.minAge = 18;
+    this.userParams.maxAge = 99;
+    this.loadUsers();
+  }
+
+  loadUsers(): void {
+    this.userService.getUsers(this.pagination.currentPage, this.pagination.itemsPerPage, this.userParams)
+      .subscribe((res: PaginationResult<User[]>) => {
+        this.users = res.result;
+        this.pagination = res.pagination;
+      }, error => {
+        this.alertifyService.error(error);
+      });
+  }
 
 }
